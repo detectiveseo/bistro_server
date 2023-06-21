5246682f06dc01cc25b81c278756c06afe3b62bd
detectiveseo <detectiveseo1@gmail.com>
2023-06-21 19:45:18 +0600

    added delete add to cart fucntion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -13,51 +13,69 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.darcm8e.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
-  async function run() {
-    try {
-      await client.connect();
-      const menuCollection = client.db("DB_BOSS").collection("menus");
-      const add_to_cartCollection = client.db("DB_BOSS").collection("addToCart")
-
-      app.get("/menus", async(req, res) => {
-        const menus = await menuCollection.find().toArray();
-        res.send(menus);
-      })
-
-      app.post("/add-to-cart", (req, res) => {
-        const body = req.body;
-        const result = add_to_cartCollection.insertOne(body)
-        res.send(result)
-      })
-
-     app.get("/added-items/:email", async(req, res) => {
-      const email = req.params.email;
-      const query = {email: email};
-      const result = await add_to_cartCollection.find(query).toArray();
-      res.send(result);
-     })
-    
-
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-    //   await client.close();
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  run().catch(console.dir);
+});
 
-  
+async function run() {
+  try {
+    await client.connect();
+    const menuCollection = client.db("DB_BOSS").collection("menus");
+    const add_to_cartCollection = client.db("DB_BOSS").collection("addToCart")
+
+    app.get("/menus", async (req, res) => {
+      const menus = await menuCollection.find().toArray();
+      res.send(menus);
+    })
+
+    app.post("/add-to-cart", async (req, res) => {
+      const body = req.body;
+      const result = await add_to_cartCollection.insertOne(body)
+      console.log(result)
+      res.send(result)
+    })
+
+    app.get("/added-items/:email", async (req, res) => {
+      const email = req.params.email;
+      const limit = Math.round(req.query.limit) || 5;
+      const page = Math.round(req.query.page) || 1;
+      const skip = (page - 1) * limit;
+      const query = { email: email };
+      const result = await add_to_cartCollection.find(query).limit(limit).skip(skip).toArray();
+      res.send(result);
+    })
+
+    app.get("/total-added-food", async (req, res) => {
+      const email = req.query?.email;
+      const result = await add_to_cartCollection.find({ email: email }).toArray();
+      res.send(result);
+    })
+
+
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await add_to_cartCollection.deleteOne({});
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 app.listen(port, () => {
-    console.log(`your port is runing on this ${port}`)
+  console.log(`your port is runing on this ${port}`)
 })
 
